db885cb4c24197a43934aa3027af710dcdaf2592
taminhnhat <taminhnhat98@gmail.com>
2023-08-03 10:41:21 +0700

    i

--- launch/ld19.launch.py
+++ launch/ld19.launch.py
@@ -32,38 +32,38 @@ Parameter Description:
       which is [135.0, 225.0], angle unit is degress.
 '''
 
+
 def generate_launch_description():
-  # LDROBOT LiDAR publisher node
-  ldlidar_node = Node(
-      package='ldlidar_stl_ros2',
-      executable='ldlidar_stl_ros2_node',
-      name='LD19',
-      output='screen',
-      parameters=[
-        {'product_name': 'LDLiDAR_LD19'},
-        {'topic_name': 'scan'},
-        {'port_name': '/dev/usb/hiddev0'},
-        {'frame_id': 'base_laser'},
-        {'laser_scan_dir': True},
-        {'enable_angle_crop_func': False},
-        {'angle_crop_min': 135.0},
-        {'angle_crop_max': 225.0}
-      ]
-  )
+    # LDROBOT LiDAR publisher node
+    ldlidar_node = Node(
+        package='ldlidar_stl_ros2',
+        executable='ldlidar_stl_ros2_node',
+        name='LD19',
+        output='screen',
+        parameters=[
+            {'product_name': 'LDLiDAR_LD19'},
+            {'topic_name': 'scan'},
+            {'port_name': '/dev/ldlidar'},
+            {'frame_id': 'base_laser'},
+            {'laser_scan_dir': True},
+            {'enable_angle_crop_func': False},
+            {'angle_crop_min': 135.0},
+            {'angle_crop_max': 225.0}
+        ]
+    )
 
-  # base_link to base_laser tf node
-  base_link_to_laser_tf_node = Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='base_link_to_base_laser_ld19',
-    arguments=['0','0','0.18','0','0','0','base_link','base_laser']
-  )
+    # base_link to base_laser tf node
+    base_link_to_laser_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_to_base_laser_ld19',
+        arguments=['0', '0', '0.18', '0', '0', '0', 'base_link', 'base_laser']
+    )
 
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
 
-  # Define LaunchDescription variable
-  ld = LaunchDescription()
+    ld.add_action(ldlidar_node)
+    ld.add_action(base_link_to_laser_tf_node)
 
-  ld.add_action(ldlidar_node)
-  ld.add_action(base_link_to_laser_tf_node)
-
-  return ld
+    return ld
